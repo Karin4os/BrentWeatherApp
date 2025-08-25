258e93200a347895f56f7ea893d5ce85917c2ace
Karina Kuteliya <karina.kuteliya@argusmedia.com>
2025-08-25 22:57:22 +0400

    date format added

--- streamlit/app.py
+++ streamlit/app.py
@@ -5,6 +5,7 @@ from prophet import Prophet
 
 # Load data
 DB_PATH = "data/data.db"  # path inside /app in container
+# DB_PATH = "../data/data.db"
 conn = sqlite3.connect(DB_PATH)
 prices = pd.read_sql("SELECT * FROM commodity_prices", conn)
 weather = pd.read_sql("SELECT * FROM weather_data", conn)
@@ -18,7 +19,10 @@ st.subheader("Weather Trend")
 st.line_chart(weather.set_index("date"))
 
 st.subheader("Price Forecast")
+# Convert '1960M01' to a datetime (first day of month)
 df = prices.rename(columns={prices.columns[0]: "ds", prices.columns[1]: "y"})
+# Convert '1960M01' to datetime (first day of month)
+df["ds"] = pd.to_datetime(df["ds"], format="%YM%m")
 model = Prophet()
 model.fit(df)
 future = model.make_future_dataframe(periods=90)
